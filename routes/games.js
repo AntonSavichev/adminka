9d8610b07dc16bd8375282d335a53c44cfbb1dd9
AntonSavichev <anthony.savi4ev@yandex.ru>
2024-05-02 20:29:31 +0300

    final

--- routes/games.js
+++ routes/games.js
@@ -1,9 +1,10 @@
 const gamesRouter = require("express").Router(); // Создали роутер
-const {sendAllGames, deleteGame,addGameController} = require('../controllers/games');
-const getAllGames = require("../middlewares/games")
+const {sendAllGames} = require('../controllers/games');
+const {getAllGames, updateGameFile, findGameById, sendUpdatedGames,  deleteGame, checkGameinArray, updateGamesArray } = require("../middlewares/games");
+
 
 gamesRouter.get("/games", getAllGames, sendAllGames)
-gamesRouter.delete("/games/:id", getAllGames, deleteGame);
-gamesRouter.post("/games", getAllGames, addGameController)
+gamesRouter.delete("/games/:id", getAllGames, findGameById, deleteGame, updateGameFile, sendUpdatedGames);
+gamesRouter.post("/games", getAllGames, checkGameinArray, updateGamesArray, updateGameFile, sendUpdatedGames)
 
 module.exports = gamesRouter; 
